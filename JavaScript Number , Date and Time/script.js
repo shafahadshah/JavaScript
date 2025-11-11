16c55a2c776b987ab38862ef117e1e33fe4cfbc8
shafahadshah <pirshahfahadshah@gmail.com>
2025-11-11 16:01:36 +0500

    Js Number and Its Property 1st Part

---
+++ JavaScript Number , Date and Time/script.js
@@ -0,0 +1,50 @@
+//  Number
+let num = 123456;
+console.log(num);
+console.log(typeof num);  //Typeof Number
+
+let num3 = Number(123456);
+console.log(num3);
+console.log(typeof num3);  //Typeof Number
+
+let num4 = Number('123456');
+console.log(num4);
+console.log(typeof num4);  //Typeof Number
+
+let num1 = 1.234567;
+console.log(num1);
+console.log(typeof num1); //Typeof Number
+
+let numn = "1234567";
+console.log(numn);
+console.log(typeof numn); //Typeof String
+
+//  Litral Method
+let number = 123456;
+console.log(number);
+
+// Constructor Method
+let num2 = new Number(123456);
+console.log(num2);  
+ 
+// Number Properties
+// Max Value
+console.log(Number.MAX_VALUE); // 1.7976931348623157e+308
+
+//Min Value
+console.log(Number.MIN_VALUE); // 5e-324
+
+//Positive Infinity
+console.log(Number.POSITIVE_INFINITY); //infinity
+
+//Negitive Infinity
+console.log(Number.NEGATIVE_INFINITY); //-infinity
+
+//NaN
+console.log(Number.NaN); //NaN
+
+//isNaN
+console.log(Number.isNaN); //isNaN
+
+
+
